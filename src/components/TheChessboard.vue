f48b5c83c7fa2681086aca74f2723a1c7c14ce2f
Jose Doñas <jose.gonzalez.donas@gmail.com>
2025-12-06 16:25:24 +0100

    feat: highlight squares on click

--- src/components/TheChessboard.vue
+++ src/components/TheChessboard.vue
@@ -1,17 +1,43 @@
 <script setup lang="ts">
-const NUMBER_OF_SQUARES = 64
+import { useGameStore } from '@/stores/game'
+import { ref } from 'vue'
 
-const isDarkSquare = (index: number) => ((index % 2) + Math.ceil(index / 8)) % 2
+const NUMBER_OF_FILES = 8
+const NUMBER_OF_RANKS = 8
+const NUMBER_OF_SQUARES = NUMBER_OF_FILES * NUMBER_OF_RANKS
+
+const clickedSquares = ref<Array<boolean>>([])
+
+const { addMove } = useGameStore()
+
+const isDarkSquare = (index: number) => {
+  const file = Math.floor(index / NUMBER_OF_FILES)
+  const rank = index % NUMBER_OF_RANKS
+
+  return file % 2 !== rank % 2
+}
+
+const onSquareClicked = (index: number) => {
+  // Store the move
+  addMove(index)
+
+  // Mark square as clicked
+  clickedSquares.value[index] = true
+}
 </script>
 
 <template>
-  <div class="w-full grid grid-cols-8 border border-black">
+  <div class="w-full grid grid-cols-8 border border-black cursor-pointer">
     <div
-      v-for="value in NUMBER_OF_SQUARES"
-      v-bind:key="value"
+      v-for="(_, index) in NUMBER_OF_SQUARES"
+      v-bind:key="index"
       class="col-span-1 aspect-square"
-      :class="{ 'bg-green-600': isDarkSquare(value) }"
+      :class="{
+        'bg-green-400': isDarkSquare(index),
+        'bg-green-700': clickedSquares[index] && isDarkSquare(index),
+        'bg-gray-300': clickedSquares[index] && !isDarkSquare(index),
+      }"
+      @click="onSquareClicked(index)"
     ></div>
   </div>
 </template>
-s
