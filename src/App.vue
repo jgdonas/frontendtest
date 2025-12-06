5f5b17389b75c6bf90f2c5079d7954ee84825150
Jose Doñas <jose.gonzalez.donas@gmail.com>
2025-12-06 16:25:11 +0100

    feat: make the whole app look as required

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
 import TheChessBoard from '@/components/TheChessboard.vue'
+import TheSidebar from '@/components/TheSidebar.vue'
 </script>
 
 <template>
-  <TheChessBoard></TheChessBoard>
+  <div class="h-screen w-screen flex flex-col lg:flex-row items-center p-1 gap-1">
+    <main class="w-full aspect-square flex flex-col justify-center">
+      <TheChessBoard></TheChessBoard>
+    </main>
+
+    <aside
+      class="h-full w-full lg:w-80 lg:h-auto lg:self-stretch bg-stone-700 rounded-lg shadow-xl flex flex-col overflow-y-auto"
+    >
+      <TheSidebar></TheSidebar>
+    </aside>
+  </div>
 </template>
 
 <style scoped></style>
