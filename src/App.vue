dfe6810c777901cf7b76a452556c8d28126cc155
Jose Doñas <jose.gonzalez.donas@gmail.com>
2025-12-07 10:22:34 +0100

    feat (wip): build main structure

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,15 @@
 <script setup lang="ts">
-import TheChessBoard from '@/components/TheChessboard.vue'
+import TheChessboard from '@/components/TheChessboard.vue'
 import TheSidebar from '@/components/TheSidebar.vue'
 </script>
 
 <template>
-  <div class="h-screen w-screen flex flex-col lg:flex-row items-center p-1 gap-1">
-    <main class="w-full aspect-square flex flex-col justify-center">
-      <TheChessBoard></TheChessBoard>
-    </main>
-
-    <aside
-      class="h-full w-full lg:w-80 lg:h-auto lg:self-stretch bg-stone-700 rounded-lg shadow-xl flex flex-col overflow-y-auto"
-    >
-      <TheSidebar></TheSidebar>
+  <div class="w-screen h-screen flex flex-col lg:flex-row items-center bg-stone-800">
+    <div class="w-screen">
+      <main class="aspect-square max-w-svh m-auto p-1"><TheChessboard /></main>
+    </div>
+    <aside class="w-full h-full lg:overflow-scroll">
+      <TheSidebar />
     </aside>
   </div>
 </template>
-
-<style scoped></style>
