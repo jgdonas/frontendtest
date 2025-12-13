294642c0db202bbbb720035338aee5282e9be37a
Jose Doñas <jose.gonzalez.donas@gmail.com>
2025-12-13 13:03:20 +0100

    feat: make sidebar sticky on larger devices

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,12 @@ import TheSidebar from '@/components/TheSidebar.vue'
 </script>
 
 <template>
-  <div class="w-screen h-screen flex flex-col lg:flex-row items-center bg-stone-800">
+  <div class="w-screen h-screen flex flex-col lg:flex-row items-center ">
     <div class="w-screen">
       <main class="aspect-square max-w-svh m-auto p-1"><TheChessboard /></main>
     </div>
-    <aside class="w-full h-full lg:overflow-scroll">
-      <TheSidebar />
+    <aside class="w-full h-full">
+      <TheSidebar class="h-full" />
     </aside>
   </div>
 </template>
--- src/components/TheSidebar.vue
+++ src/components/TheSidebar.vue
@@ -5,22 +5,26 @@ const { moves, reset } = useGameStore()
 </script>
 
 <template>
-  <div class="flex flex-col bg-stone-800 text-stone-200 p-4">
-    <div class="flex flex-row justify-between">
-      <h2 class="text-xl font-bold mb-4">Moves</h2>
-      <span v-if="moves.length > 0" class="select-none cursor-pointer" @click="reset()">🗑️</span>
-    </div>
+  <div class="flex flex-col  p-4">
+    <header>
+      <div class="flex flex-row justify-between">
+        <h2 class="text-xl font-bold mb-4">Moves</h2>
+        <span v-if="moves.length > 0" class="select-none cursor-pointer" @click="reset()">🗑️</span>
+      </div>
 
-    <div v-if="moves.length === 0">No moves yet...</div>
+      <div v-if="moves.length === 0">No moves yet...</div>
+    </header>
 
-    <div
-      v-for="(move, index) in moves"
-      v-bind:key="index"
-      class="flex px-3 py-2"
-      :class="index % 2 === 0 ? 'bg-stone-700' : 'bg-stone-800'"
-    >
-      <span class="text-stone-500 w-8 mr-2">{{ index + 1 }}.</span>
-      <span class="font-bold">{{ move }}</span>
+    <div class="lg:overflow-scroll flex-1">
+      <div
+        v-for="(move, index) in moves"
+        v-bind:key="index"
+        class="flex px-3 py-2"
+        :class="index % 2 === 0 ? 'bg-stone-700' : 'bg-stone-400'"
+      >
+        <span class="text-stone-500 w-8 mr-2">{{ index + 1 }}.</span>
+        <span class="font-bold">{{ move }}</span>
+      </div>
     </div>
   </div>
 </template>
